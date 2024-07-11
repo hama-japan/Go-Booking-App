02526f7f0cff41ce58fcaa0757f814dd97e377d0
xymalos <xymalos@proton.me>
2024-07-11 10:36:38 +0900

    add dataTypes

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	var conferenceName string = "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets int = 50
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
